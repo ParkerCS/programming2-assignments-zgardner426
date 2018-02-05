131fbadba2f29a56ac566e71b8967cf51f4b3ff4
zoe <zgardner@fwparker.org>
2018-02-05 08:09:21 -0600

    test commit

--- functions_and_imports.py
+++ functions_and_imports.py
@@ -4,14 +4,48 @@
 #PROBLEM 1 (how many upper case - 4pts)
 # Make a function takes a string as a parameter, then prints how many upper case letters are contained in the string.
 # A loop that compares each letter to the .upper() or .lower() of itself will suffice.
+def function_1(word):
+    count = 0
+    for letters in word:
+        if letters == letters.upper():
+            count += 1
+            if letters == " ":
+                count -= 1
+    print(count)
+
+
+function_1("Hi My name is Zoe")
 
 # PROBLEM 2 (Biggest, smallest, average - 4pts)
 # Make a function which takes 3 numbers as parameters.
 # The function then prints the largest, the smallest, and their average, rounded to 2 decimals.
 # Display the answers in a "nicely" formatted way.
 
+
+def function_2(num1, num2, num3):
+    greatest_number = max(num1, num2, num3)
+    lowest_number = min(num1, num2, num3)
+    average = (num1 + num2 + num3) / 3
+    print("Greatest Number = ", greatest_number)
+    print("Lowest Number = ", lowest_number)
+    print("Average = {:.2f}".format(average))
+
+
+function_2(189, 782, 23)
+
+
 # PROBLEM 4 (add me, multiply me - 4pts)
 # Make a function which takes in two integers and RETURNS their sum AND their product.
+
+def function_3(num1, num2):
+    sum = num1 + num2
+    product = num1 * num2
+    print("The sum of {} and {} is {}".format(num1, num2, sum))
+    print("The product of {} and {} is {}".format(num1, num2, product))
+
+
+function_3(13, 89)
+
 
 # PROBLEM 5 (Login - 4pts)
 # Make a file called import_me.py in this same project
