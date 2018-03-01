a2ad8574242c6d31b567ece8de2e57d616524101
zoe <zgardner@fwparker.org>
2018-03-01 12:47:58 -0600

    test commit

--- searching_problems.py
+++ searching_problems.py
@@ -12,17 +12,87 @@ def split_line(line):
 # word in the provided dictionary.  If there are more
 # than one longest word, print them all.  (read the file line by line to accomplish this task)
 
+
+file = open('search_files/dictionary.txt')
+
+longest_word = " "
+word_lens = []
+word_list = []
+
+
+for lines in file:
+    words = split_line(lines)
+    for word in words:
+        word_list.append(word)
+file.close()
+
+
+file = open('search_files/dictionary.txt')
+
+for lines in file:
+    words = split_line(lines)
+    for word in words:
+        word_lens.append(len(word))
+file.close()
+
+
+len_long_word = max(word_lens)
+index = word_lens.index(len_long_word)
+longest_word = word_list[index]
+print(longest_word)
+
+
 #2.  (7pts)  Write code which finds
 #  The total word count AND average word length of "AliceInWonderLand.txt"
 
+word_count = 0
+average_list = []
+average = 0
 
+file = open('search_files/AliceInWonderLand.txt')
+for lines in file:
+    words = split_line(lines)
+    for word in words:
+        word_count += 1
+        average_list.append(len(word))
+
+
+for numbers in average_list:
+    average += numbers
+
+
+average = average/(len(average_list))
+print("Word Count:", word_count, "Average word length:", average)
 
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
 
 #3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
 # How many times does "Cat" occur?
-# How many times does "Cheshire" immediately followed by "Cat" occur?
+file = open('search_files/AliceInWonderLand.txt')
+num_cat = 0
 
+
+for lines in file:
+    words = split_line(lines)
+    for word in words:
+        if word == "Cat":
+            num_cat += 1
+
+print(num_cat)
+file.close()
+
+# How many times does "Cheshire" immediately followed by "Cat" occur?
+file = open('search_files/AliceInWonderLand.txt')
+num_cheshire = 0
+
+for lines in file:
+    words = split_line(lines)
+    for word in words:
+        if word == "Cheshire":
+            num_cheshire += 1
+
+print(num_cheshire)
+file.close()
 
 #### OR #####
 
