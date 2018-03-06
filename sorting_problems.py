ffbab0e148d3a3756a3c3754572d72c93c7cadce
zoe <zgardner@fwparker.org>
2018-03-06 14:24:28 -0600

    test commit

--- sorting_problems.py
+++ sorting_problems.py
@@ -67,12 +67,13 @@ print(sort_me2)
 # you iterate through the big loop, and the inner loop of the sort.
 # Make the functions print each value.  Run the sorts on the lists below.
 
+# using the insertion sort
 sort_me3 = [77, 29, 59, 69, 86, 40, 47, 40, 74, 44, 58, 78, 9, 8, 13, 99, 3, 57, 19, 48, 55, 50, 94, 69, 98, 30, 37, 29, 40, 29, 36, 32, 26, 85, 61, 51, 70, 96, 90, 89, 91, 88, 68, 4, 4, 74, 15, 72, 5, 91, 76, 6, 56, 80, 72, 87, 63, 86, 48, 24, 17, 23, 30, 41, 7, 64, 16, 19, 40, 63, 14, 95, 44, 58, 1, 6, 45, 55, 52, 54, 44, 36, 50, 6, 96, 66, 8, 46, 48, 48, 75, 25, 39, 30, 70, 44, 38, 90, 49, 70]
 iterations_ins = insertion_sort(sort_me3)
 print(sort_me3)
 print(iterations_ins)
 
-
+# using the selection sort
 sort_me4 = [50, 61, 96, 72, 67, 12, 14, 1, 35, 51, 38, 32, 34, 29, 95, 75, 74, 83, 33, 3, 70, 0, 41, 4, 32, 1, 93, 39, 4, 20, 14, 11, 24, 69, 36, 36, 54, 90, 95, 36, 25, 24, 76, 30, 92, 95, 24, 6, 72, 78, 95, 73, 94, 33, 36, 30, 19, 23, 52, 28, 17, 82, 98, 74, 67, 43, 2, 89, 87, 8, 91, 7, 22, 78, 74, 84, 74, 87, 67, 93, 47, 74, 95, 92, 25, 46, 8, 74, 58, 80, 33, 31, 69, 2, 21, 93, 96, 72, 50, 61]
 iterations_sel = selection_sort(sort_me4)
 print(sort_me4)
